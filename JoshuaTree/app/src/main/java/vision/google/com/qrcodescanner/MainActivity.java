23abc476b56c50d33deaee8ed95fdfe95b3d9820
Andrew Phillips <ajp20@live.com>
2017-10-21 23:35:41 -0700

    map and POST (untested)

--- JoshuaTree/app/src/main/java/vision/google/com/qrcodescanner/MainActivity.java
+++ JoshuaTree/app/src/main/java/vision/google/com/qrcodescanner/MainActivity.java
@@ -16,18 +16,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.vision.barcode.Barcode;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     Button scanbtn;
     public static final int REQUEST_CODE = 100;
     public static final int PERMISSION_REQUEST = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scanbtn = (Button) findViewById(R.id.scanbtn);
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
         }
         scanbtn.setOnClickListener(new View.OnClickListener() {
@@ -41,36 +48,54 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
-            if(data != null){
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                StaticConstants.setUpMap();
                 final Barcode barcode = data.getParcelableExtra("barcode");
+                if (!StaticConstants.map.containsKey(barcode.displayValue)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("New item");
+                    final EditText input = new EditText(this);
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                    builder.setView(input);
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.i("Barcode", barcode.displayValue);
+                            Log.i("Input", input.getText().toString());
+                            StaticConstants.map.put(barcode.displayValue, input.getText().toString());
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    builder.show();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("New item");
+                    String url = "10.143.195.71:8080/ChangeInventory";
 
-                // Set up the input
-                final EditText input = new EditText(this);
-                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT);
-                builder.setView(input);
-                // Set up the buttons
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.i("Barcode", barcode.displayValue);
-                        Log.i("Input",input.getText().toString());
-                        //StaticConstants.map.put(barcode.displayValue,input.getText().toString());
+                    JSONObject jsonBody = new JSONObject();
+                    try {
+                        jsonBody.put(barcode.displayValue, input.getText().toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
 
-                builder.show();
-                
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, jsonBody, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.v("Successful POST", response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.v("Not successful POST", error.toString());
+                            error.printStackTrace();
+                        }
+                    });
+                }
             }
         }
     }
